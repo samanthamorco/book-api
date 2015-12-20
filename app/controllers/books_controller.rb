8403048d269a27a369b11a68cc2eefbee13a3432
Samantha Morco <morkotiks@gmail.com>
2015-12-19 17:37:00 -0800

    initial commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.create(title: params[:title], author_first_name: params[:author_first_name], author_last_name: params[:author_last_name], page_count: params[:page_count], rating: params[:rating], genre: params[:genre])
+    render :show
+  end
+
+  def edit
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(title: params[:title], author_first_name: params[:author_first_name], author_last_name: params[:author_last_name], page_count: params[:page_count], rating: params[:rating], genre: params[:genre])
+    render :show
+    # redirect_to "/books/#{@book.id}"
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to "/books" }
+      format.json { render json: "Book Removed." }
+    end
+  end
+end
---
+++ app/views/books/_book.json.jbuilder
@@ -0,0 +1,7 @@
+  json.title book.title
+  json.author_first_name book.author_first_name
+  json.author_last_name book.author_last_name
+  json.page_count book.page_count
+  json.rating book.rating
+  json.genre book.genre
+  json.id book.id
